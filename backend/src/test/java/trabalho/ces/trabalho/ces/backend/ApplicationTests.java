f15a2121639eb2a5ae73b867bbdd159f7e3014fc
Lucas Almeida <magmapt@gmail.com>
2020-03-07 02:05:14 -0300

    Projeto de frontend e backend adicionados

---
+++ backend/src/test/java/trabalho/ces/trabalho/ces/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalho.ces.trabalho.ces.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
