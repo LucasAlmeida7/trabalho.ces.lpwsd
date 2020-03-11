3d996a044f5b370ca013a602f4bcd91bd460fc42
Lucas Almeida <magmapt@gmail.com>
2020-03-11 00:54:54 -0300

    Configurações do vue e componente de header

---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,10 @@
+<template src="./Header.html"></template>
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style lang="scss" src="./Header.scss" scoped></style>
